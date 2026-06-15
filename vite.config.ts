fcac144e7103620ea76492842ceeb5af946e5b22
ksenia <ksikorskaya.reon@gmail.com>
2026-06-15 23:05:32 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES === 'true' ? '/sboard_skia_test/' : '/',
+  base: process.env.GITHUB_PAGES === 'true' ? '/canvas-renderer-lab/' : '/',
 });
